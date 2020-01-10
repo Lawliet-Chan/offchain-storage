03501f06e51c783cfce23a89436cb27b27096f50
CrocdileChan <CrocdileChan@github.com>
2020-01-10 14:11:47 +0800

    complete test

--- frame/offchain-storage/src/lib.rs
+++ frame/offchain-storage/src/lib.rs
@@ -1,58 +1,56 @@
 use codec::{Decode, Encode};
-use frame_support::{
-	decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult
-};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult};
 use frame_system::{self as system, ensure_signed};
-use sp_std::{vec::Vec, default::Default};
+use sp_std::{default::Default, vec::Vec};
 
 // ExternalStorage is for developers to implement specific storage
 // such as ipfs, mysql, mongodb, neo4j and so on.
 pub trait ExternalStorage {
-	fn get(key: Vec<u8>) -> Vec<u8>;
-	fn set(key: Vec<u8>, value: Vec<u8>);
-	fn delete(key: Vec<u8>);
+    fn get(key: Vec<u8>) -> Vec<u8>;
+    fn set(key: Vec<u8>, value: Vec<u8>);
+    fn delete(key: Vec<u8>);
 }
 
 pub trait Trait: frame_system::Trait {
-	/// The overarching event type.
-	type Event: From<Event> + Into<<Self as frame_system::Trait>::Event>;
-	/// External storage service.
-	type Storage: ExternalStorage;
+    /// The overarching event type.
+    type Event: From<Event> + Into<<Self as frame_system::Trait>::Event>;
+    /// External storage service.
+    type Storage: ExternalStorage;
 }
 
 #[derive(Encode, Decode, Clone, Default, PartialEq)]
 pub struct UserData<AccountId> {
-	// the author means this data was created by this person.
-	// author has the Write access.
-	author: AccountId,
+    // the author means this data was created by this person.
+    // author has the Write access.
+    author: AccountId,
 
-	access: Access,
+    access: Access,
 }
 
 /// Access is that the access of UserData.
 #[derive(Encode, Decode, Clone, PartialEq)]
 pub enum Access {
-	// Avoid means that no one can read or write this data unless author.
-	Avoid,
-	// Read means that this data just can be read.
-	Read,
-	// Write means that every one can read and write this data.
-	Write,
+    // Avoid means that no one can read or write this data unless author.
+    Avoid,
+    // Read means that this data just can be read.
+    Read,
+    // Write means that every one can read and write this data.
+    Write,
 }
 
 impl Default for Access {
-	fn default() -> Self{
-		Access::Read
-	}
+    fn default() -> Self {
+        Access::Read
+    }
 }
 
 // for the convenience of comparing access.
 fn access_value(ac: Access) -> u8 {
-	match ac {
-		Access::Avoid => 0,
-		Access::Read => 1,
-		Access::Write => 2,
-	}
+    match ac {
+        Access::Avoid => 0,
+        Access::Read => 1,
+        Access::Write => 2,
+    }
 }
 
 decl_event! {
@@ -144,100 +142,106 @@ decl_module! {
 }
 
 impl<T: Trait> Module<T> {
-	// check user's operation access
-	fn check_op_access(user: T::AccountId, data: UserData<T::AccountId>, op: Access) -> bool {
-		/// User must have a higher access level than the data has.
-		/// Or the user is author itself.
-		access_value(data.access) >= access_value(op) || user == data.author
-	}
+    // check user's operation access
+    fn check_op_access(user: T::AccountId, data: UserData<T::AccountId>, op: Access) -> bool {
+        /// User must have a higher access level than the data has.
+        /// Or the user is author itself.
+        access_value(data.access)
+            >= access_value(op)
+            || user == data.author
+    }
 
-	fn get_external_storage(data_id: Vec<u8>) -> Vec<u8> {
-		T::Storage::get(data_id)
-	}
+    fn get_external_storage(data_id: Vec<u8>) -> Vec<u8> {
+        T::Storage::get(data_id)
+    }
 
-	fn set_external_storage(data_id: Vec<u8>, data: Vec<u8>) {
-		T::Storage::set(data_id, data)
-	}
+    fn set_external_storage(data_id: Vec<u8>, data: Vec<u8>) {
+        T::Storage::set(data_id, data)
+    }
 
-	fn delete_external_storage(data_id: Vec<u8>) {
-		T::Storage::delete(data_id)
-	}
+    fn delete_external_storage(data_id: Vec<u8>) {
+        T::Storage::delete(data_id)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	use frame_support::{impl_outer_origin, assert_ok, parameter_types, weights::Weight};
+    use frame_support::{assert_ok, impl_outer_origin, parameter_types, weights::Weight};
 
-	impl_outer_origin! {
-		pub enum Origin for Test {}
-	}
+    impl_outer_origin! {
+        pub enum Origin for Test {}
+    }
 
-	// For testing the module, we construct most of a mock runtime. This means
-	// first constructing a configuration type (`Test`) which `impl`s each of the
-	// configuration traits of modules we want to use.
-	#[derive(Clone, Eq, PartialEq)]
-	pub struct Test;
-	parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: Weight = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-	}
-	impl system::Trait for Test {
-		type Origin = Origin;
-		type Call = ();
-		type Index = u64;
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = ();
-		type BlockHashCount = BlockHashCount;
-		type MaximumBlockWeight = MaximumBlockWeight;
-		type MaximumBlockLength = MaximumBlockLength;
-		type AvailableBlockRatio = AvailableBlockRatio;
-		type Version = ();
-		type ModuleToIndex = ();
-	}
-	impl Trait for Test {
-		type Event = ();
-		type Storage = HttpDB;
-	}
+    // For testing the module, we construct most of a mock runtime. This means
+    // first constructing a configuration type (`Test`) which `impl`s each of the
+    // configuration traits of modules we want to use.
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Test;
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: Weight = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    }
+    impl system::Trait for Test {
+        type Origin = Origin;
+        type Call = ();
+        type Index = u64;
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type BlockHashCount = BlockHashCount;
+        type MaximumBlockWeight = MaximumBlockWeight;
+        type MaximumBlockLength = MaximumBlockLength;
+        type AvailableBlockRatio = AvailableBlockRatio;
+        type Version = ();
+        type ModuleToIndex = ();
+    }
+    impl Trait for Test {
+        type Event = ();
+        type Storage = HttpDB;
+    }
 
-	use sp_runtime::offchain::http::Request;
+    use sp_runtime::offchain::http::Request;
 
-	// Simulate a external database.
-	pub struct HttpDB;
+    // Simulate a external database.
+    pub struct HttpDB;
 
-	impl ExternalStorage for HttpDB {
-		fn get(key: Vec<u8>) -> Vec<u8> {
-			let req: Request = Request::get("http://localhost:1234");
-			let pending = req.send().unwrap();
-			let mut response = pending.wait().unwrap();
-			let body = response.body();
-			body.collect::<Vec<u8>>()
-		}
+    impl ExternalStorage for HttpDB {
+        fn get(key: Vec<u8>) -> Vec<u8> {
+            let req: Request = Request::get("http://localhost:1234");
+            let pending = req.send().unwrap();
+            let mut response = pending.wait().unwrap();
+            let body = response.body();
+            body.collect::<Vec<u8>>()
+        }
 
-		fn set(key: Vec<u8>, value: Vec<u8>) {
-			let req: Request = Request::post("http://localhost:1234");
-			let pending = req.send().unwrap();
-			let mut response = pending.wait().unwrap();
-		}
+        fn set(key: Vec<u8>, value: Vec<u8>) {
+            let req: Request = Request::post("http://localhost:1234");
+            let pending = req.send().unwrap();
+            let mut response = pending.wait().unwrap();
+        }
 
-		fn delete(key: Vec<u8>) {
-			let req: Request = Request::delete("http://localhost:1234");
-			let pending = req.send().unwrap();
-		}
-	}
+        fn delete(key: Vec<u8>) {
+            let req: Request = Request::delete("http://localhost:1234");
+            let pending = req.send().unwrap();
+        }
+    }
 
-	type OffchainStorage = Module<Test>;
+    type OffchainStorage = Module<Test>;
 
-	#[test]
-	fn do_external_storage() {
-
-	}
+    #[test]
+    fn do_external_storage() {
+        let key: Vec<u8> = b"key".to_vec();
+        let value: Vec<u8> = b"key".to_vec();
+        OffchainStorage::write_data(Origin::signed(1), key, value);
+        assert_eq!(OffchainStorage::read_data(Origin::signed(2), key), value);
+        OffchainStorage::delete_data(Origin::signed(1), key);
+    }
 }
